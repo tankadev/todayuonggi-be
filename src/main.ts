0c9dcbf87f9987c3309ad994be2ef514c28658ce
tankadev <nguyentankadev@gmail.com>
2021-06-13 18:59:39 +0700

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const whitelist = ['localhost:4200', 'todayuonggi.vercel.app'];
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  });
   await app.listen(process.env.PORT || 6000);
 }
 bootstrap();
